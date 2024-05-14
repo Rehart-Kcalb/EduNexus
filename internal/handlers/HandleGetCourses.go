8973976d77058898e1fe34bed88527bdba4a440f
Rehart-Kcalb <dead.nitst@gmail.com>
2024-05-14 22:56:02 +0500

    added pagination for api/courses

--- internal/handlers/HandleGetCourses.go
+++ internal/handlers/HandleGetCourses.go
@@ -2,7 +2,9 @@ package handlers
 
 import (
 	"context"
+	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/Rehart-Kcalb/EduNexus-Monolith/internal/db"
 	"github.com/Rehart-Kcalb/EduNexus-Monolith/internal/types"
@@ -10,7 +12,23 @@ import (
 
 func HandleGetCourses(DB *db.Queries) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		courses, err := DB.GetCourses(context.Background(), db.GetCoursesParams{Limit: 100, Offset: 0})
+		err := r.ParseForm()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		Limit_str := r.FormValue("perPage")
+		Offset_str := r.FormValue("page")
+		limit_num, err := strconv.Atoi(Limit_str)
+		if err != nil {
+			log.Println(err)
+		}
+		Offset_num, err := strconv.Atoi(Offset_str)
+		if err != nil {
+			log.Println(err)
+		}
+		Offset_num = (Offset_num - 1) * (limit_num)
+		courses, err := DB.GetCourses(context.Background(), db.GetCoursesParams{Limit: int32(limit_num), Offset: int32(Offset_num)})
 		if err != nil {
 			types.NewJsonResponse("Problem with DB", http.StatusInternalServerError).Respond(w)
 			return
